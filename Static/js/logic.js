a9eea07f8a711742ee0f4553c7512d9c755304d5
Sanjaye Thomas <sanjaye22@gmail.com>
2020-09-10 22:16:20 -0400

    js updates

--- Static/js/logic.js
+++ Static/js/logic.js
@@ -12,11 +12,10 @@ d3.csv("../../Data/FinalCleanedData.csv", function(ACAData) {
   // Adding a tile layer (the background map image) to our map
   // We use the addTo method to add objects to our map
   var light = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
+    id: "mapbox/light-v10",
+    tileSize: 512,
     zoomOffset: -1,
-    id: "mapbox/light-v10,
     accessToken: API_KEY
-  }).addTo(myMap);
-  
+  });
