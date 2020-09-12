8e56197e73d0df3c181231766d8d3bc5c039ba99
Sanjaye Thomas <sanjaye22@gmail.com>
2020-09-12 14:28:44 -0400

    updates

--- Resources/aca.sql
+++ Resources/aca.sql
@@ -12,9 +12,9 @@ CREATE TABLE aca_table (
     "Death Per 1000 (2015)" INT,
 	"Latitude" INT,
     "Longitude" INT,
-    "Uninsured Rate (2010)" INT,
-	"Uninsured Rate (2015)" INT,
-    "Uninsured Rate Change (2010-2015)" INT
+    "Uninsured Rate % (2010)" INT,
+	"Uninsured Rate % (2015)" INT,
+    "Uninsured Rate Change % (2010-2015)" INT
 );
 
 -- View table
