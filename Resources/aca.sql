1cfc28e9b30975e3a6abfc108d66e344edd868e0
Sanjaye Thomas <sanjaye22@gmail.com>
2020-09-13 21:37:02 -0400

    updates

--- Resources/aca.sql
+++ Resources/aca.sql
@@ -4,18 +4,23 @@ DROP TABLE aca_table;
 --Create Table
 CREATE TABLE aca_table (
     "State" VARCHAR,
-    "Deaths (2010)" INT,
-    "Births (2010)" INT,
-    "Death Per 1000 (2010)" INT,
-	"Deaths (2015)" INT,
-	"Births (2015)" INT,
-    "Death Per 1000 (2015)" INT,
+    "Deaths_2010" INT,
+    "Births_2010" INT,
+    "Death_Per_1000_2010" INT,
+	"Deaths_2015" INT,
+	"Births_2015" INT,
+    "Death_Per_1000_2015" INT,
 	"Latitude" INT,
     "Longitude" INT,
-    "Uninsured Rate % (2010)" INT,
-	"Uninsured Rate % (2015)" INT,
-    "Uninsured Rate Change % (2010-2015)" INT
+    "Uninsured_Rate_2010(%))" INT,
+	"Uninsured_Rate_2015(%))" INT,
+    "Uninsured_Rate_Change_2010-2015(%)" INT
 );
 
 -- View table
 SELECT * FROM aca_table;
+
+--Querty infor for State
+Select "State", "Deaths_2010", "Deaths_2015", "Births_2010", "Births_2015"
+FROM aca_table
+
